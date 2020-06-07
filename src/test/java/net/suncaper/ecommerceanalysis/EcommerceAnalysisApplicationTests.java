a0589ced5b4be1585877ec7d7af9eec8fd682560
mleage <jiadian0728@163.com>
2020-06-07 19:17:02 +0800

    first commiy

---
+++ src/test/java/net/suncaper/ecommerceanalysis/EcommerceAnalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package net.suncaper.ecommerceanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceAnalysisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
