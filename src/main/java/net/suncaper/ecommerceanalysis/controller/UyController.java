22d3a05c326e7bea5b14e39522598eb0d8d5f239
darlingof02 <295226786@qq.com>
2020-06-11 13:47:17 +0800

    漏斗图可视化完成

--- src/main/java/net/suncaper/ecommerceanalysis/controller/UyController.java
+++ src/main/java/net/suncaper/ecommerceanalysis/controller/UyController.java
@@ -1,5 +1,32 @@
 package net.suncaper.ecommerceanalysis.controller;
 
-public class UyController {
 
+
+import net.suncaper.ecommerceanalysis.domain.UvDetailDay;
+import net.suncaper.ecommerceanalysis.domain.UvDetailMonth;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
+import java.util.Map;
+
+@Controller
+@RequestMapping("/uy")
+public class UyController {
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @GetMapping("funnel")
+    @ResponseBody
+    public List<Map<String,Object>> funnel() {
+        String sql = "SELECT * FROM loudou_analysis ORDER BY total DESC";
+
+        List<Map<String,Object>> funnelResults = jdbcTemplate.queryForList(sql);
+
+        return funnelResults;
+    }
 }
--- src/main/java/net/suncaper/ecommerceanalysis/domain/Funnel.java
+++ src/main/java/net/suncaper/ecommerceanalysis/domain/Funnel.java
@@ -1,4 +1,22 @@
 package net.suncaper.ecommerceanalysis.domain;
 
 public class Funnel {
+    private int action;
+    private int total;
+
+    public int getAction() {
+        return action;
+    }
+
+    public int getTotal() {
+        return total;
+    }
+
+    public void setAction(int action) {
+        this.action = action;
+    }
+
+    public void setTotal(int total) {
+        this.total = total;
+    }
 }
